e68c7fc368ca1436b4b70eeb94ae145c7b1148d9
Deepak Sisodiya <deepak.sisodiya@gmail.com>
2020-04-11 11:24:23 +0530

    type inference

--- app.js
+++ app.js
@@ -4,3 +4,9 @@ function add(n1, n2) {
 var number1 = 5;
 var number2 = 10;
 console.log(add(number1, number2));
+/*
+const n1 = 5;
+typescript does type inference and know the type of n1 is number only
+so we need not to do this in typescript
+const n1: number = 5;
+*/
--- app.ts
+++ app.ts
@@ -6,3 +6,10 @@ const number1 = 5;
 const number2 = 10;
 
 console.log(add(number1, number2));
+
+/*
+const n1 = 5;
+typescript does type inference and know the type of n1 is number only
+so we need not to do this in typescript
+const n1: number = 5;
+*/
