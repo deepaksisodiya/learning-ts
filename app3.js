c66605fee7bd523256d0991af076fd4935ed8987
Deepak Sisodiya <deepak.sisodiya@gmail.com>
2020-04-11 11:38:29 +0530

    typescript type array of string

---
+++ app3.js
@@ -0,0 +1,17 @@
+// typescript array type
+var Person = {
+    name: 'Deepak',
+    age: 30,
+    hobbies: ['Cooking', 'Coding']
+};
+/*
+typescript does type infernece and know that hobbies is of type array of string
+*/
+var activity;
+activity = ['Cooking', 'Coding'];
+for (var _i = 0, _a = Person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
+    // due to type inference vs code know that you can perform all string oepration on hobby
+    // console.log(hobby.map()); // gives an ERROR because map is not performed on hobby of type string
+}
---
+++ app3.ts
@@ -0,0 +1,20 @@
+// typescript array type
+
+const Person = {
+  name: 'Deepak',
+  age: 30,
+  hobbies: ['Cooking', 'Coding']
+};
+
+/*
+typescript does type infernece and know that hobbies is of type array of string
+*/
+
+let activity: string[];
+activity = ['Cooking', 'Coding'];
+
+for (const hobby of Person.hobbies) {
+  console.log(hobby.toUpperCase());
+  // due to type inference vs code know that you can perform all string oepration on hobby
+  // console.log(hobby.map()); // gives an ERROR because map is not performed on hobby of type string
+}
