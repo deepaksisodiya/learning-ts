1f30be6621a0f62838ca1a1af6fc2af0da65bad2
Deepak Sisodiya <deepak.sisodiya@gmail.com>
2020-05-01 13:27:03 +0530

    typescript never type

---
+++ app11.ts
@@ -0,0 +1,7 @@
+// typescript never type
+
+function generateError(message: string, code: number): never {
+  throw { message: message, errorCode: code }
+}
+
+generateError('An error occurred', 500);
