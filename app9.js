37671c46edf7fbd8d4f6048ec0b3c047c2573a5f
Deepak Sisodiya <deepak.sisodiya@gmail.com>
2020-05-01 12:42:06 +0530

    function type

--- app9.js
+++ app9.js
@@ -2,6 +2,7 @@
 function add(n1, n2) {
     return n1 + n2;
 }
+// let combineValues: Function;
 var combineValues;
 // combineValues = 5; this will give error
 combineValues = add;
--- app9.ts
+++ app9.ts
@@ -4,7 +4,8 @@ function add(n1: number, n2: number) {
   return n1 + n2;
 }
 
-let combineValues: Function;
+// let combineValues: Function;
+let combineValues: (a1: number, a2: number) => number;
 // combineValues = 5; this will give error
 
 combineValues = add;
