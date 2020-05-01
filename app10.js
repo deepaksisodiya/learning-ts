4c5c6926965d18e1e9a947dd5c2f192987ccf980
Deepak Sisodiya <deepak.sisodiya@gmail.com>
2020-05-01 13:18:38 +0530

    unknown type

---
+++ app10.js
@@ -0,0 +1,7 @@
+var userInput;
+var userName;
+userInput = 5;
+userInput = 'input';
+if (typeof userInput === 'string') {
+    userName = userInput;
+}
---
+++ app10.ts
@@ -0,0 +1,9 @@
+let userInput: unknown;
+let userName: string;
+
+userInput = 5;
+userInput = 'input';
+
+if (typeof userInput === 'string') {
+  userName = userInput
+}
